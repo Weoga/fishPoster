18c054a543658d61d5c47d312e90b5e7f421ad2d
Weoga <iliazel8@gmail.com>
2024-01-11 15:27:24 +0200

    added newpost function

--- main.py
+++ main.py
@@ -3,33 +3,34 @@ import praw
 import json
 
 with open('login.json') as f:
-    login = json.load(f)
+	login = json.load(f)
 
 reddit = praw.Reddit(
-    client_id=login['client_id'],
-    client_secret=login['client_secret'],
-    redirect_uri=login['redirect_uri'],
-    user_agent=login['user_agent'],
-    username=login['username'],
-    password=login['password'],
+	client_id=login['client_id'],
+	client_secret=login['client_secret'],
+	redirect_uri=login['redirect_uri'],
+	user_agent=login['user_agent'],
+	username=login['username']
 )
-bot = telebot.TeleBot("6744314989:AAENrlOqZOi-7lojTqaD5VuTnmNGQKbbb90")
+bot = telebot.TeleBot(login['tg_id'])
+subreddit_name = 'cats'
 
-channel_id = "@marcestest"
-def imFish():
-	bot.send_message("@hnshgd", "я рыбка")
+@bot.message_handler(commands=['start'])
+def start(message):
+	bot.send_message(message.chat.id, 'hi mom')
 
 
-def catpost():
-	subreddit = reddit.subreddit('cats')
+@bot.message_handler(commands=['newpost'])
+def newpost(message):
+	subreddit = reddit.subreddit(subreddit_name)
 	hot = subreddit.new()
 	for post in hot:
 		image_link = post.url if ('.png' in post.url or '.jpg' in post.url or '.jpeg' in post.url) else None
 		if image_link:
 			title = post.title
 			print(post.title)
-			# print(post.caption)
-			bot.send_photo(chat_id=channel_id, photo=image_link, caption=title)
+			bot.send_photo(chat_id=message.chat.id, photo=image_link, caption=title)
 
-if __name__ == "__main__":
-	catpost()
+
+if __name__ == '__main__':
+	bot.polling()
